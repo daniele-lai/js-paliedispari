71f36d3219d3d08465f66dc046f1760ca542699e
Daniele <danielelai86@gmail.com>
2021-03-08 16:28:33 +0100

    Fine esercizio palindromi

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -4,16 +4,25 @@ Creare una funzione per capire se la parola inserita è palindroma*/
 // 1. Chiediamo all'utente di inserire una parola
 var parola = prompt("Inserisci una parola");
 
-//2 Creo un ciclo for per la parola da sinistra verso destra
-for (var i = 0; i < parola.length; i++) {
-  var parola1 = parola[i]
-  console.log(parola1);
+//2. Creo una funzione per capire se la funzione è palindroma
+function palindroma(parola) {
+  //3. Creo un ciclo for per la parola da sinistra verso destra
+  for (var i = 0; i < parola.length; i++) {
+    var parola1 = parola[i];
+  }
+  //4. Creo un ciclo for per la parola da destra verso sinistra
+  var parola2 = "";
+
+  for (var i = parola.length - 1; i >= 0; i--) {
+    parola2 = parola[i];
+  }
+  /*5. Creo un if per verificare se le parole nell'ordine inverso combaciano*/
+  if (parola1 !== parola2) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
-//Creo un ciclo for per la parola da destra verso sinistra
-var parola2 = "";
-
-for (var i = parola.length - 1; i >= 0; i--) {
-  parola2 = parola[i];
-  console.log(parola2);
-}
+var risultato = palindroma(parola);
+console.log(risultato);
